5dced588e57c5c2b73159c833511fd375bd05c91
YernarT <toktaryernar@gmail.com>
2023-04-28 09:08:51 +0600

    wip: :constructor: 读写本地文件

--- lib/pages/list_page/index.dart
+++ lib/pages/list_page/index.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
 
+// Store
+import 'package:get/get.dart';
+import 'package:easy_note/store/getx/note_controller.dart';
+import 'package:easy_note/store/file_system.dart' show getFiles, getFile;
+
+// Model
 // import 'package:easy_note/models/note_model.dart';
 
+// Component
 import 'package:easy_note/components/search.dart';
 import 'package:easy_note/components/fab.dart';
 // import 'package:easy_note/components/note_card.dart';
 
 import 'package:easy_note/pages/list_page/mock.dart' show getNoteList;
 
-class ListPage extends StatelessWidget {
+class ListPage extends StatefulWidget {
   const ListPage({super.key});
+
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  final NoteController noteController = Get.find();
+  // ?
+  bool isLoading = true;
+
+  // print('appDocDir: ');
+  // print(appDocDir);
+
+  @override
+  void initState() {
+    super.initState();
+
+    getFiles().then((value) {
+      print('files: ');
+      print(value);
+      print(noteController.currentNoteIndex.value);
+      print(noteController.noteList.value);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          floatingActionButton: const FAB(),
+          floatingActionButton: FAB(),
           body: Padding(
             padding: const EdgeInsets.all(16),
             child: Column(children: [
